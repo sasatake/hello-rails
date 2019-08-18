d9eb33fb3789b97b5385bd7f7b1f2e6bd7553c27
sasatake <t06418as@gmail.com>
2019-08-18 00:48:46 +0000

    add request spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
+
+    it "returns status code 200" do
       get "/users"
       expect(response).to have_http_status(200)
     end
+
+    it "returns empty user list" do
+      get "/users"
+      expect(response.parsed_body['users'].size).to eq(0)
+    end
+
   end
 end
