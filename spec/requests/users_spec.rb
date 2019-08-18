6d64ee76d696e8fbfa5d13d56c6acf6528e6829e
sasatake <t06418as@gmail.com>
2019-08-18 01:15:43 +0000

    modify users request spec with FactoryBot

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
 
+    let(:user_amount) { 20 }
+
+    before do
+      create_list(:user, user_amount)
+    end
+
     it "returns status code 200" do
       get "/users"
       expect(response).to have_http_status(200)
@@ -10,7 +16,7 @@ RSpec.describe "Users", type: :request do
 
     it "returns empty user list" do
       get "/users"
-      expect(response.parsed_body['users'].size).to eq(0)
+      expect(response.parsed_body['users'].size).to eq(user_amount)
     end
 
   end
